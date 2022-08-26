1a15d750d8d48eb398b433ee2421ecc98a20a737
Demon154 <chriswhitelol77@gmail.com>
2022-08-26 21:25:23 +0500

    add conditional.js

---
+++ conditional.js
@@ -0,0 +1,23 @@
+// Conditional //
+
+// the if statement //
+// lets take an example for if statement
+//  if the hour is less than 18 then it's a good day
+let hour = 9;
+
+if (hour < 18) {
+  console.log("its a good day");
+}
+// switch statement
+x = "0";
+switch (x) {
+  case "0":
+    console.log("Off");
+    break;
+  case "1":
+    console.log("On");
+    break;
+  default:
+    console.log("bruh");
+    break;
+}
